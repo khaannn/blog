6ddaab2b4aa6d1c8b095381dd365c47966e27fb1
Chatham_J <jeff.chatham@dep.state.fl.us>
2016-03-31 16:48:31 -0400

    Couchbase arrives

---
+++ src/main/java/com/khaannn/couchbase/Config.java
@@ -0,0 +1,31 @@
+package com.khaannn.couchbase;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.couchbase.config.AbstractCouchbaseConfiguration;
+import org.springframework.data.couchbase.repository.config.EnableCouchbaseRepositories;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by chatham_j on 3/31/2016.
+ */
+@Configuration
+@EnableCouchbaseRepositories
+public class Config extends AbstractCouchbaseConfiguration {
+
+    @Override
+    protected List<String> getBootstrapHosts() {
+        return Arrays.asList("localhost:5984");
+    }
+
+    @Override
+    protected String getBucketName() {
+        return "example";
+    }
+
+    @Override
+    protected String getBucketPassword() {
+        return "";
+    }
+}
---
+++ src/main/java/com/khaannn/couchbase/repository/Post.java
@@ -0,0 +1,10 @@
+package com.khaannn.couchbase.repository;
+
+/**
+ * Created by chatham_j on 3/31/2016.
+ * http://guide.couchdb.org/draft/views.html
+ * http://projects.spring.io/spring-data-couchbase/
+ *
+ */
+public class Post {
+}
